7dc5c86c0516452d913d4d56c55d77fb0ee429a2
kxwzhang <kxwzhang@gmail.com>
2020-12-18 12:03:02 -0800

    Style basic Logout button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { PrivateRoute, RestrictedRoute } from './components/util/route_util';
 import { Splash } from './components/Splash';
+import { Profile } from './components/Profile';
 
 function App() {
   return (
     <div>
       <Switch>
+        <PrivateRoute exact path='/profile' component={Profile} />
         <Route exact path='/' component={Splash} />
 
       </Switch>
---
+++ src/components/Logout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+import { useAuth0 } from '@auth0/auth0-react';
+
+export const Logout = () => {
+  return (
+    <div>
+      <LogoutButton>Logout</LogoutButton>
+    </div>
+  )
+}
+
+const LogoutButton = styled.button`
+  border: none;
+  background-color: gray;
+  color: white;
+  transition: background-color 0.75s, color 0.75s;
+
+  &:hover {
+    background-color: red;
+    color: black;
+  }
+`;
