47582ca050dd924266d75c7b7197f3fc8fbae9c7
kxwzhang <kxwzhang@gmail.com>
2020-12-18 11:40:57 -0800

    Destructure user profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export const Profile = () => {
+  const { user } = useAuth0();
   return (
-    <div>
-      
-    </div>
+    <FlexWrapper>
+      <ProfileImage src={user.picture}/>
+      <h2>{user.name}</h2>
+      <h3>{user.email}</h3>
+    </FlexWrapper>
   )
 }
+
+const FlexWrapper = styled.div`
+  display: flex;
+`;
+
+const ProfileImage = styled.img`
+  &:hover {
+    border: 1px solid blue;
+  }
+`;
