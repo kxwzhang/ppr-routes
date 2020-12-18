3609c202cf64f646d4bbf045b95089ad85bb49e3
kxwzhang <kxwzhang@gmail.com>
2020-12-17 19:22:41 -0800

    Add ModalBackground and ModalChild styled-comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
-// import styled from 
+import styled from 'styled-components';
+
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: #000000b3;
+  z-index: 9000;
+  display: flex;
+`;
+
+const ModalChild = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  box-shadow: 50px;
+  border-radius: 2%;
+`;
 
 export const Login = () => {
   return (
     <div>
-      
+      <ModalBackground>
+        <ModalChild>
+          
+        </ModalChild>
+      </ModalBackground>
     </div>
   )
 }
