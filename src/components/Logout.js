6f1cf0b421cffc83e558b1c296909929bfd41c13
kxwzhang <kxwzhang@gmail.com>
2020-12-18 12:03:55 -0800

    Add the logout event on the logout button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export const Logout = () => {
+  const { logout } = useAuth0();
   return (
     <div>
-      <LogoutButton>Logout</LogoutButton>
+      <LogoutButton onClick={() => logout()}>Logout</LogoutButton>
     </div>
   )
 }
