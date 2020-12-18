68294d3b557dd174e3d0e1b9335c44eeaa8fa0cc
kxwzhang <kxwzhang@gmail.com>
2020-12-18 12:06:47 -0800

    Embed logout into splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Login } from './Login';
+import { Logout } from './Logout';
 import { Modal } from './Modal';
 
 export const Splash = () => {
   const [modal, setModal] = useState(false);
+  const { isAuthenticated } = useAuth0();
   return (
     <div>
       <div>Auth0 Test</div>
-      <Login modal={modal} setModal={setModal} />
+      {!isAuthenticated && <Login modal={modal} setModal={setModal} />}
+      {isAuthenticated && <Logout />}
       {modal && <Modal modal={modal} setModal={setModal} />}
     </div>
   )
