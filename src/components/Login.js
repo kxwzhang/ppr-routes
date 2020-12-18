1a017fbc2ac9b4ad8cb2349dd90e38e323645638
kxwzhang <kxwzhang@gmail.com>
2020-12-17 20:09:06 -0800

    Add a button on splash to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,51 +1,14 @@
-import React, { useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react'
 import styled from 'styled-components';
 
 export const Login = ({ modal, setModal }) => {
-  const { loginWithRedirect } = useAuth0();
   return (
     <div>
-      <ModalBackground onClick={() => setModal(false)}>
-        <ModalChild>
-          <div>Please Sign In with Auth0</div>
-          <Auth0Button onClick={() => loginWithRedirect()}>Sign In</Auth0Button>
-        </ModalChild>
-      </ModalBackground>
+      <LoginButton onClick={() => setModal(true)}>Login</LoginButton>
     </div>
   )
 }
 
-const ModalBackground = styled.div`
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  background: #000000b3;
-  z-index: 9000;
-  display: flex;
-`;
-
-const ModalChild = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: white;
-  box-shadow: 50px;
-  border-radius: 2%;
-`;
-
-const Auth0Button = styled.button`
+const LoginButton = styled.button`
   border: none;
-  color: black;
-  background-color: gray;
-  transition: background-color 0.75s, color 0.75s;
-
-  &:hover {
-    cursor: pointer;
-    color: white:
-    background-color: blue;
-  }
 `;
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Login } from './Login';
+import { Modal } from './Modal';
 
 export const Splash = () => {
   const [modal, setModal] = useState(false);
@@ -7,6 +8,7 @@ export const Splash = () => {
     <div>
       <div>Auth0 Test</div>
       <Login modal={modal} setModal={setModal} />
+      {modal && <Modal modal={modal} setModal={setModal} />}
     </div>
   )
 }
