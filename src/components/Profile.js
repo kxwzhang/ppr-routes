d50f4416d6c9d3de05340e5d98ca4b19f6278b38
kxwzhang <kxwzhang@gmail.com>
2020-12-19 13:35:05 -0800

    Add CDN for Tachyons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,10 +6,10 @@ import { useAuth0 } from '@auth0/auth0-react';
 export const Profile = withRouter(({ history }) => {
   const { user } = useAuth0();
   return (
-    <FlexWrapper>
-      <ProfileImage src={user.picture}/>
-      <h2>{user.name}</h2>
-      <h3>{user.email}</h3>
+    <FlexWrapper className='pa4 tc'>
+      <img className='br-100 ba h3 w3 dib' src={user.picture}/>
+      <h2 className=''>{user.name}</h2>
+      <h3 className=''>{user.email}</h3>
       <HomePageButton onClick={() => history.push('/')}>
         Home
       </HomePageButton>
@@ -24,8 +24,8 @@ const FlexWrapper = styled.div`
 `;
 
 const ProfileImage = styled.img`
-  border: 1px solid black;
-  border-radius: 50%;
+  ${'' /* border: 1px solid black;
+  border-radius: 50%; */}
 `;
 
 const HomePageButton = styled.button`
