e8ca38e9dff9ff0953fe2e70cba1229b714814cc
kxwzhang <kxwzhang@gmail.com>
2020-12-24 11:56:29 -0800

    Add Header Content and Footer, and style position fixed on Header

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { Layout, Menu, Breadcrumb } from 'antd'
+const { Header, Content, Footer } = Layout
 
 export const NavigationBar = () => {
   return (
     <div>
-      
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+
+        </Header>
+      </Layout>
     </div>
   )
 }
