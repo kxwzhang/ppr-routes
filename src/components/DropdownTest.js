1785f412355dc6bcc090cda4cf3fe654f996ecc7
kxwzhang <kxwzhang@gmail.com>
2021-01-02 11:00:25 -0800

    import Dropdown from antd

---
+++ src/components/DropdownTest.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Menu, Dropdown, Button, message, Space, Tooltip } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+
+
+export const DropdownTest = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
