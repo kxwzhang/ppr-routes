b91225906b9a2223eaf382ef7282165ab9121920
kxwzhang <kxwzhang@gmail.com>
2021-01-02 11:00:56 -0800

    Write handleClicks

--- src/components/DropdownTest.js
+++ src/components/DropdownTest.js
@@ -2,6 +2,15 @@ import React from 'react'
 import { Menu, Dropdown, Button, message, Space, Tooltip } from 'antd';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
 
+function handleButtonClick(e) {
+  message.info('Click on left button.');
+  console.log('click left button', e);
+}
+
+function handleMenuClick(e) {
+  message.info('Click on menu item.');
+  console.log('click', e);
+}
 
 export const DropdownTest = () => {
   return (
