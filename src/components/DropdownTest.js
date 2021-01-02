2eb19aa2b4dcd6b0de9f33e167512c2d6929526a
kxwzhang <kxwzhang@gmail.com>
2021-01-02 11:01:55 -0800

    Add Menu

--- src/components/DropdownTest.js
+++ src/components/DropdownTest.js
@@ -12,6 +12,20 @@ const handleMenuClick = (e) => {
   console.log('click', e)
 }
 
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="1" icon={<UserOutlined />}>
+      1st menu item
+    </Menu.Item>
+    <Menu.Item key="2" icon={<UserOutlined />}>
+      2nd menu item
+    </Menu.Item>
+    <Menu.Item key="3" icon={<UserOutlined />}>
+      3rd menu item
+    </Menu.Item>
+  </Menu>
+)
+
 export const DropdownTest = () => {
   return (
     <div>
