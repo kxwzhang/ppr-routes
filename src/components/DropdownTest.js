dfa50c53b6b21e3f1491d44f07c92356852d022d
kxwzhang <kxwzhang@gmail.com>
2021-01-02 11:01:32 -0800

    Convert to arrow func

--- src/components/DropdownTest.js
+++ src/components/DropdownTest.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Menu, Dropdown, Button, message, Space, Tooltip } from 'antd';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
+import { Menu, Dropdown, Button, message, Space, Tooltip } from 'antd'
+import { DownOutlined, UserOutlined } from '@ant-design/icons'
 
-function handleButtonClick(e) {
-  message.info('Click on left button.');
-  console.log('click left button', e);
+const handleButtonClick = (e) => {
+  message.info('Click on left button.')
+  console.log('click left button', e)
 }
 
-function handleMenuClick(e) {
-  message.info('Click on menu item.');
-  console.log('click', e);
+const handleMenuClick = (e) => {
+  message.info('Click on menu item.')
+  console.log('click', e)
 }
 
 export const DropdownTest = () => {
